5c6d85b40bc0bdfd070f4af7fdb0726f0827b9af
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2024-02-22 15:06:57 +0300

    change sql

--- ffnn.py
+++ ffnn.py
@@ -27,10 +27,10 @@ def test_predict(model_name):
                           password=config_db.PASSWORD,
                           dsn=config_db.DSN) as connection:
         with connection.cursor() as cursor:
-            for r in cursor.execute(SQL.get_predict_select()):
+            for r in cursor.execute(sql.get_predict_select()):
                 predict_result = predict(model_name, r[0])
                 with connection.cursor() as cursor_upd:
-                    cursor_upd.execute(SQL.get_predict_update(),
+                    cursor_upd.execute(sql.get_predict_update(),
                                        class_check_id=int(predict_result[0]),
                                        record_id=int(r[1]))
                     connection.commit()
