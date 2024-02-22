cb00cb846bec1bbcebf0273c1d7fb7016b0dd894
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2024-02-22 15:16:50 +0300

    change sql

--- ffnn.py
+++ ffnn.py
@@ -17,6 +17,7 @@ def predict(model_name, predict_data):
 
     predict_arr = tf.stack(predict_arr)
 
+    print(predict_arr)
     model = tf.keras.models.load_model(model_name)
     return list(map(np.argmax, model.predict(predict_arr)))
 
