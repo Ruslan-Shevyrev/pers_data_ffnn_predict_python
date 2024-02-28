0f052315978cd298a20839022052dc94603ea5fa
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2024-02-28 14:32:01 +0300

    change SQL

--- sql.py
+++ sql.py
@@ -1,7 +1,14 @@
+'''
 PREDICT_SELECT = ("SELECT VAL, RECORD_ID "
                   "FROM DBRECORDS "
                   "WHERE CLASS_CHECK_ID IS NULL "
                   "AND VAL = CONVERT(VAL,'UTF8')")
+'''
+
+
+PREDICT_SELECT = ("SELECT VAL, RECORD_ID "
+                  "FROM TABLE(PKG_CLEAR_DATA.GET_DBRECORDS)")
+
 
 PREDICT_UPDATE = ("UPDATE DBRECORDS "
                   "SET CLASS_CHECK_ID = :class_check_id "
