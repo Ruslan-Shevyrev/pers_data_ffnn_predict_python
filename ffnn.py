bd8363f7957f78f60b5703dbf8c7ad8657cf7c8b
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2024-03-21 12:39:28 +0300

    container

--- ffnn.py
+++ ffnn.py
@@ -4,10 +4,30 @@ import utils
 import oracledb
 import config.config_db as config_db
 import sql
+import os
+
+os.environ['CUDA_VISIBLE_DEVICES'] = ''
 
 
-def predict(model_name, predict_data):
+def predict(model, predict_data):
     predict_arr = list()
+    if type(predict_data) is list:
+        predict_arr = predict_data
+    else:
+        predict_arr.append(predict_data)
+
+    predict_arr = list(map(utils.str_to_arr, predict_arr))
+
+    predict_arr = tf.stack(predict_arr)
+
+    return list(map(np.argmax, model.predict(predict_arr)))
+
+
+def predict_model_name(model_name, predict_data):
+    model = tf.keras.models.load_model(model_name)
+    return predict(model, predict_data)
+
+    '''predict_arr = list()
     if type(predict_data) is list:
         predict_arr = predict_data
     else:
@@ -19,15 +39,29 @@ def predict(model_name, predict_data):
 
     model = tf.keras.models.load_model(model_name)
     return list(map(np.argmax, model.predict(predict_arr)))
+    '''
 
 
 def test_predict(model_name):
+
+    sql.CONTAINERS_COUNT = os.getenv('CONTAINERS_COUNT', '')
+    sql.CONTAINER_NUM = os.getenv('CONTAINERS_NUM', '')
+
+    model = tf.keras.models.load_model(model_name)
+
     with oracledb.connect(user=config_db.USER,
                           password=config_db.PASSWORD,
                           dsn=config_db.DSN) as connection:
         with connection.cursor() as cursor:
+            if sql.CONTAINERS_COUNT != '' and sql.CONTAINER_NUM != '':
+                for r in cursor.execute(sql.get_container_records_count(),
+                                        containers_cnt=int(sql.CONTAINERS_COUNT),
+                                        container_num=int(sql.CONTAINER_NUM)):
+                    sql.RECORDS_OFFSET = str(r[0])
+                    sql.RECORDS_COUNT = str(r[1])
+
             for r in cursor.execute(sql.get_predict_select()):
-                predict_result = predict(model_name, r[0])
+                predict_result = predict(model, r[0])
                 with connection.cursor() as cursor_upd:
                     cursor_upd.execute(sql.get_predict_update(),
                                        class_check_id=int(predict_result[0]),
--- sql.py
+++ sql.py
@@ -1,10 +1,7 @@
-'''
-PREDICT_SELECT = ("SELECT VAL, RECORD_ID "
-                  "FROM DBRECORDS "
-                  "WHERE CLASS_CHECK_ID IS NULL "
-                  "AND VAL = CONVERT(VAL,'UTF8')")
-'''
-
+CONTAINERS_COUNT = ''
+CONTAINER_NUM = ''
+RECORDS_OFFSET = ''
+RECORDS_COUNT = ''
 
 PREDICT_SELECT = ("SELECT VAL, RECORD_ID "
                   "FROM TABLE(PKG_CLEAR_DATA.GET_DBRECORDS)")
@@ -14,10 +11,21 @@ PREDICT_UPDATE = ("UPDATE DBRECORDS "
                   "SET CLASS_CHECK_ID = :class_check_id "
                   "WHERE RECORD_ID = :record_id")
 
+CONTAINER_RECORDS_COUNT = ("SELECT (trunc((max(RECORD_ID) - min(RECORD_ID)) / :containers_cnt) + 1) * (:container_num - 1) AS RECORDS_OFFSET, "
+                           "(trunc((max(RECORD_ID) - min(RECORD_ID)) / :containers_cnt) + 1) * :container_num AS RECORDS_COUNT "
+                           "FROM TABLE(PKG_CLEAR_DATA.GET_DBRECORDS) ")
+
 
 def get_predict_select():
-    return PREDICT_SELECT
+    if RECORDS_OFFSET != '' and RECORDS_COUNT != '':
+        return PREDICT_SELECT + " OFFSET "+RECORDS_OFFSET+" ROWS FETCH NEXT "+RECORDS_COUNT+" ROWS ONLY"
+    else:
+        return PREDICT_SELECT
 
 
 def get_predict_update():
     return PREDICT_UPDATE
+
+
+def get_container_records_count():
+    return CONTAINER_RECORDS_COUNT
