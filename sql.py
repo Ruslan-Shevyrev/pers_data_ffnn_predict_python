3d49b39c72345b67fcbfae6ae336f5db38cc9038
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2024-03-28 12:59:40 +0300

    exception

--- sql.py
+++ sql.py
@@ -3,17 +3,23 @@ CONTAINER_NUM = ''
 RECORDS_OFFSET = ''
 RECORDS_COUNT = ''
 
+'''
 PREDICT_SELECT = ("SELECT VAL, RECORD_ID "
                   "FROM TABLE(PKG_CLEAR_DATA.GET_DBRECORDS)")
+'''
+
+PREDICT_SELECT = ("SELECT VAL, RECORD_ID "
+                  "FROM V_DBRECORDS_FOR_PREDICT")
 
 
 PREDICT_UPDATE = ("UPDATE DBRECORDS "
                   "SET CLASS_CHECK_ID = :class_check_id "
                   "WHERE RECORD_ID = :record_id")
 
+
 CONTAINER_RECORDS_COUNT = ("SELECT (trunc((max(RECORD_ID) - min(RECORD_ID)) / :containers_cnt) + 1) * (:container_num - 1) AS RECORDS_OFFSET, "
                            "(trunc((max(RECORD_ID) - min(RECORD_ID)) / :containers_cnt) + 1) * :container_num AS RECORDS_COUNT "
-                           "FROM TABLE(PKG_CLEAR_DATA.GET_DBRECORDS(nREG_EXP => 0)) ")
+                           "FROM V_DBRECORDS_FOR_PREDICT")
 
 
 def get_predict_select():
